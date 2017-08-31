c2e451833001e1639d53ec55c76df1e50b4b4f93
Sebastian Colorado <sbsclrdo@gmail.com>
2017-08-31 11:02:33 -0400

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,7 @@
+import twitter
+
+
+api = twitter.Api(consumer_key=CONSUMER_KEY,
+                  consumer_secret=CONSUMER_SECRET,
+                  access_token_key=ACCESS_TOKEN,
+                  access_token_secret=ACCESS_TOKEN_SECRET)
