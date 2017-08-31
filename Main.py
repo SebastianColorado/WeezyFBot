f644a60cfa8a38a4bd1f56115fd5f1cb3893394b
Sebastian Colorado <sbsclrdo@gmail.com>
2017-08-31 14:05:08 -0400

    Get keys from env variables and added a test tweet

--- Main.py
+++ Main.py
@@ -1,7 +1,9 @@
 import twitter
+import os
 
+api = twitter.Api(consumer_key=os.environ['CONSUMER_KEY'],
+                  consumer_secret=os.environ['CONSUMER_SECRET'],
+                  access_token_key=os.environ['ACCESS_TOKEN'],
+                  access_token_secret=os.environ['ACCESS_TOKEN_SECRET'])
 
-api = twitter.Api(consumer_key=CONSUMER_KEY,
-                  consumer_secret=CONSUMER_SECRET,
-                  access_token_key=ACCESS_TOKEN,
-                  access_token_secret=ACCESS_TOKEN_SECRET)
+status = api.PostUpdate('Weezy F. Baby and the F is for Fracking')
