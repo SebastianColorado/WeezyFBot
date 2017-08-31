4a8815fb3b8bec052d5a2669db18e55d808c20cd
Sebastian Colorado <sbsclrdo@gmail.com>
2017-08-31 15:10:05 -0400

    added some error checking for posting the tweet

--- Main.py
+++ Main.py
@@ -2,10 +2,19 @@
 
 import twitter
 import os
+import sys
 
 api = twitter.Api(consumer_key=os.environ['CONSUMER_KEY'],
                   consumer_secret=os.environ['CONSUMER_SECRET'],
                   access_token_key=os.environ['ACCESS_TOKEN'],
                   access_token_secret=os.environ['ACCESS_TOKEN_SECRET'])
 
-status = api.PostUpdate('Weezy F. Baby and the F is for Fracking')
+try:
+        status = api.PostUpdate('Weezy F. Baby and the F is for Foodie')
+except UnicodeDecodeError:
+        print("Your message could not be encoded. Perhaps it contains \
+        non-ASCII characters?")
+        print("Try explicitly specifying the encoding with \
+        the --encoding flag")
+        sys.exit(2)
+print("%s just posted: %s" % (status.user.name, status.text))
