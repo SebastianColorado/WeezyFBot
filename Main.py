2b88c6c6133a24ea3adfe9a81333c1470bc4bace
Sebastian Colorado <sbsclrdo@gmail.com>
2017-09-12 16:31:39 -0400

    Added comments documenting the code

--- Main.py
+++ Main.py
@@ -1,59 +1,79 @@
 """Twitter bot that tweets out "Weezy F Baby and the F is for" + a word."""
 
+# Import necessary libraries
 import twitter
-from wordnik import *
+from wordnik import swagger, WordsApi
 import os
 import random
 import time
 from collections import deque
 
+# Authenticate the twitter bot by passing the twitter api keys retrieved from
+# environment variables
 twitterApi = twitter.Api(consumer_key=os.environ['CONSUMER_KEY'],
                          consumer_secret=os.environ['CONSUMER_SECRET'],
                          access_token_key=os.environ['ACCESS_TOKEN'],
                          access_token_secret=os.environ['ACCESS_TOKEN_SECRET'])
 
+# Authenticate wordnik api with api key retrieved from environment variables
 apiUrl = 'http://api.wordnik.com/v4'
 apiKey = os.environ['WORDNIK_KEY']
 wordnikApi = swagger.ApiClient(apiKey, apiUrl)
 wordsAPI = WordsApi.WordsApi(wordnikApi)
 
+# Define variations of the phrase used in the tweet
 phraseVariations = ['Weezy F. Baby and the F is for',
                     'Weezy F., the F is for',
                     'Weezy F. and the F is for',
                     'Weezy F. Baby, the F is for']
 
+# Create a deque that will hold the last 5 trending topics that have been
+# tweeted out
 lastTrendsTweeted = deque(maxlen=5)
 
 while True:
-
+    # Choose a random variation from the variations defined above
     startingPhrase = random.choice(phraseVariations)
 
+    # Retrieve trending topics from the USA and from the entire world and
+    # append the two lists together
     trends = twitterApi.GetTrendsWoeid(23424977)+twitterApi.GetTrendsCurrent()
+
+    # Pick the first trend that starts with f and isn't one of the last 5
+    # trends tweeted
     for trend in trends:
         if trend.name not in lastTrendsTweeted and \
           (trend.name.startswith("f") or trend.name.startswith("#f") or
            trend.name.startswith("F") or trend.name.startswith("#F")):
 
+            # Attempt to post the tweet and add the trend to the list of
+            # topics tweeted recently
             try:
                 status = twitterApi.PostUpdate(startingPhrase +
                                                ' ' + trend.name)
+                print("%s just posted trend: %s" % (status.user.name,
+                                                    status.text))
+                # Note: when a deque with a max length is full, appending
+                # on one side will remove the object at the end of the
+                # opposite side before adding the new object
+                lastTrendsTweeted.appendleft(trend.name)
+
             except twitter.error.TwitterError as e:
                 print('There was an error: ' + e.message[0]['message'])
-                break
-
-            print("%s just posted trend: %s" % (status.user.name, status.text))
-            lastTrendsTweeted.appendleft(trend.name)
-
+                continue
+            # Sleep for 3 hours after tweeting
             time.sleep(60*60*3)
             break
 
-    query = random.choices(['f', 'ph'], [30, 1], k=1)[0]
-
+    # Pick f or ph randomly with a higher probability of picking f
+    firstLetter = random.choices(['f', 'ph'], [30, 1], k=1)[0]
+    # Randomly pick a part of speech to search for
     partOfSpeech = random.choices(["noun", "adjective", "verb-transitive"],
                                   [7000, 2000, 400],
                                   k=1)[0]
-
-    if query == 'f':
+    # Define totals for each combination of first letter and part of speech.
+    # Hardcoded to avoid making two api calls for each word
+    if firstLetter == 'f':
         if partOfSpeech == "noun":
             total = 5436
         elif partOfSpeech == "adjective":
@@ -68,19 +88,22 @@ while True:
         else:
             total = 19
 
-    searchResults = wordsAPI.searchWords(query,
+    # Make api call for a random word
+    searchResults = wordsAPI.searchWords(firstLetter,
                                          includePartOfSpeech=partOfSpeech,
                                          caseSensitive=False,
                                          skip=random.randrange(1, total),
                                          limit=1)
-
+    # Get the word from the response
     word = searchResults.searchResults[0].word
 
+    # Attempt to post the tweet and add the trend to the list of
+    # topics tweeted recently
     try:
         status = twitterApi.PostUpdate(startingPhrase +
                                        ' ' + trend.name)
     except twitter.error.TwitterError as e:
-        print('There was an error:' + e.message[0]['message'])
+        print('There was an error: ' + e.message[0]['message'])
         continue
 
     print("%s just posted: %s" % (status.user.name, status.text))
