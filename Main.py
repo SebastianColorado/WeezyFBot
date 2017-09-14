255d9cdefdac95bb752d58d0283bee6f5602df16
Sebastian Colorado <sbsclrdo@gmail.com>
2017-09-14 15:49:53 -0400

    Made posting a trend vs a word random and refactored some code

--- Main.py
+++ Main.py
@@ -29,31 +29,19 @@ def postTweet(startPhrase, endPhrase):
 
     except twitter.error.TwitterError as e:
         print('There was an error: ' + e.message[0]['message'])
-        return (False, None)
 
     else:
         print("%s just posted: %s" % (status.user.name, status.text))
-        return (True, endPhrase)
+
+    return
 
 
-# Define variations of the phrase used in the tweet
-phraseVariations = ['Weezy F. Baby and the F is for',
-                    'Weezy F., the F is for',
-                    'Weezy F. and the F is for',
-                    'Weezy F. Baby, the F is for']
-
-# Create a deque that will hold the last 5 trending topics that have been
-# tweeted out
-lastTrendsTweeted = deque(maxlen=5)
-
-while True:
-    # Choose a random variation from the variations defined above
-    startingPhrase = random.choice(phraseVariations)
-
+def getTrend():
+    """Get the first trending topic that starts with f."""
     # Retrieve trending topics from the USA and from the entire world and
-    # append the two lists together
-    trends = twitterApi.GetTrendsWoeid(23424977)+twitterApi.GetTrendsCurrent()
-
+    # concatenate the two lists together
+    trends = twitterApi.GetTrendsWoeid(23424977) +\
+        twitterApi.GetTrendsCurrent()
     # Pick the first trend that starts with f and isn't one of the last 5
     # trends tweeted
     for trend in trends:
@@ -61,28 +49,22 @@ while True:
           (trend.name.startswith("f") or trend.name.startswith("#f") or
            trend.name.startswith("F") or trend.name.startswith("#F")):
 
-            # Attempt to post the tweet and add the trend to the list of
-            # topics tweeted recently
-            result = postTweet(startingPhrase, trend.name)
+            return trend.name
 
-            if result[0]:
-                # Note: when a deque with a max length is full, appending
-                # on one side will remove the object at the end of the
-                # opposite side before adding the new object
-                lastTrendsTweeted.appendleft(result[1])
-                # Sleep for 2 hours after tweeting
-                time.sleep(60*60*2)
+    return
 
-            break
 
+def getWord():
+    """Get random word starting with f (or occasionally ph)."""
     # Pick f or ph randomly with a higher probability of picking f
-    firstLetter = random.choices(['f', 'ph'], [30, 1], k=1)[0]
+    firstLetter = random.choices(['f', 'ph'], [30, 1])[0]
+
     # Randomly pick a part of speech to search for
     partOfSpeech = random.choices(["noun", "adjective", "verb-transitive"],
-                                  [7000, 2000, 400],
-                                  k=1)[0]
-    # Define totals for each combination of first letter and part of speech.
-    # Hardcoded to avoid making two api calls for each word
+                                  [7000, 2000, 400])[0]
+
+    # Define totals for each combination of first letter and part of
+    # speech. Hardcoded to avoid making two api calls for each word
     if firstLetter == 'f':
         if partOfSpeech == "noun":
             total = 5436
@@ -106,7 +88,39 @@ while True:
                                          limit=1)
     # Get the word from the response
     word = searchResults.searchResults[0].word
+    return word
 
-    postTweet(startingPhrase, word)
 
+# Define variations of the phrase used in the tweet
+phraseVariations = ['Weezy F Baby and the F is for',
+                    'Weezy F, the F is for',
+                    'Weezy F and the F is for',
+                    'Weezy F Baby, the F is for']
+
+# Create a deque that will hold the last 5 trending topics that have been
+# tweeted out
+lastTrendsTweeted = deque(maxlen=5)
+
+while True:
+    # Choose a random variation from the variations defined above
+    startingPhrase = random.choice(phraseVariations)
+
+    # Randomly choose wether to post a word or a trend
+    trendOrWord = random.choices(['Trend', 'Word'], [2, 3])[0]
+
+    if trendOrWord == 'Trend':
+
+        endingPhrase = getTrend()
+        # Note: when a deque with a max length is full, appending
+        # on one side will remove the object at the end of the
+        # opposite side before adding the new object
+        lastTrendsTweeted.appendleft(endingPhrase)
+
+    else:
+        endingPhrase = getWord()
+
+    # Attempt to post the tweet
+    postTweet(startingPhrase, endingPhrase)
+
+    # Sleep after posting a tweet
     time.sleep(60*60*2)
